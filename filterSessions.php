98acb8d82b0ca9ee856831b658961d0c99c32a55
LukeSchmidt1 <124911642+LukeSchmidt1@users.noreply.github.com>
2025-05-06 13:47:00 -0400

    Add files via upload

--- filterSessions.php
+++ filterSessions.php
@@ -7,33 +7,17 @@ $search = isset($_POST['search']) ? $_POST['search'] : '';
 
 if ($search == '') {
     $query = "SELECT * FROM `Sessions`";
+    $params = []; 
 } else {
-    $query = "SELECT * FROM `Sessions` WHERE `name` LIKE :search OR `Class_name` LIKE :search";
+    $query = "SELECT * FROM `Sessions` WHERE `name` LIKE :search";
+    $params = ['search' => "%$search%"];  
 }
 
 $pdo = connectDB();
 $stmt = $pdo->prepare($query);
 
-$stmt->execute(['search' => "%$search%"]);
+$stmt->execute($params);
 $results = $stmt->fetchAll();
-
-echo <<<EOT
-<div class="container">
-    <label for="toppings">Toppings:</label><br>
-    <input type="text" id="toppings" onkeyup="searchByToppings(this)" name="toppings"><br><br>
-    <p id="info"></p>
-    <table class="table table-striped table-bordered mt-4">
-        <thead class="table-dark">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Class</th>
-                <th>Time</th>
-                <th>Location</th>
-            </tr>
-        </thead>
-        <tbody>
-EOT;
 
 foreach ($results as $item) {
     echo <<<EOT
@@ -46,33 +30,4 @@ foreach ($results as $item) {
     </tr>
 EOT;
 }
-
-echo <<<EOT
-        </tbody>
-    </table>
-</div>
-EOT;
 ?>
-
-<script type="text/javascript">
-
-    document.getElementById('toppings').addEventListener('input', function () {
-        const query = this.value;
-
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'filterSessions.php', true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-        xhr.onload = function () {
-            if (this.status === 200) {
-
-                document.querySelector('table').innerHTML = this.responseText;
-            } else {
-                alert('Failed to fetch sessions.');
-            }
-        };
-        xhr.send('search=' + encodeURIComponent(query));
-    });
-</script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
